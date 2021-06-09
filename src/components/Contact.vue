0d17026031adb815a430a599cd5c46652cc1c378
julia1450 <julia1450@naver.com>
2021-06-09 16:19:27 +0900

    메일을 보낼 수 있는 contact 섹션을 구성한다

---
+++ src/components/Contact.vue
@@ -0,0 +1,95 @@
+<template>
+    <section class="section section--contact">
+        <h1 class="section-title">contact</h1>
+        <div class="contact-wrap">
+            <div>
+                <div class="form-control"><input type="text" name="sender" id="sender" placeholder="이름을 입력해주세요"></div>
+                <div class="form-control form-inline">
+                    <select class="inline-item w-30p">
+                        <option value="개인">개인</option>
+                        <option value="회사">회사</option>
+                    </select>
+                    <input class="inline-item w-left" type="text" name="company" id="company" placeholder="회사명을 입력해주세요" disabled>
+                </div>
+                <div class="form-control"><input type="text" name="subject" id="subject" placeholder="제목을 입력해주세요"></div>
+            </div>
+            <div class="form-control"><input type="text" name="message" id="message" placeholder="메세지를 입력해주세요"></div>
+        </div>
+        <button class="send-btn">send</button>
+    </section>
+</template>
+
+<script>
+export default {
+    name: "Contact"
+}
+</script>
+
+<style scoped>
+.contact-wrap {
+    margin: 0 10px;
+    display: flex;
+    height: 100%;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+}
+.contact-wrap > div {
+    width: 30%;
+    min-width: 400px;
+    padding: 0 5px;
+    height: 100%;
+}
+.contact-wrap > div:last-child {
+    margin-right: 0;
+}
+
+.contact-wrap .form-control:last-child {
+    margin-bottom: 0;
+}
+.form-control {
+    margin-bottom: 10px;
+    height: calc(100% / 3 - 10px);
+}
+.form-control > input {
+    width: 100%;
+    box-sizing: border-box;
+    height: 34px;
+    border: 2px solid #e6e6e6;
+    border-radius: 5px;
+    box-shadow: 2px 2px 2px 2px #e6e6e6;
+}
+.form-control > select {
+    border: 2px solid #e6e6e6;
+    border-radius: 5px;
+    box-shadow: 2px 2px 2px 2px #e6e6e6;
+}
+.form-control.form-inline {
+    display: flex;
+    width: 100%;
+}
+
+.inline-item.w-30p {
+    width: 30%;
+    margin-right: 5px;
+}
+
+.inline-item.w-left {
+    flex: 1;
+}
+
+input[name=message] {
+    height: calc(34px * 3 + 20px);
+}
+input:disabled {
+    cursor: not-allowed;
+}
+.send-btn {
+    height: 34px;
+    width: 70px;
+    margin-top: 20px;
+    background-color: #e6e6e6;
+    border: none;
+    border-radius: 5px;
+}
+</style>
