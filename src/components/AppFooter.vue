4ad623ae11ae4edda84958ab6474cd3a3962c6e9
julia1450 <julia1450@naver.com>
2021-06-09 16:16:16 +0900

    포트폴리오 하단에 페이지 스크롤 링크를 추가한다.

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,40 @@
+<template>
+    <section class="section section--footer">
+        <ul class="menu">
+            <li class="active"><a @click="scrollPage('page-about')" href="javascript:void(0);">about</a></li>
+            <li class=""><a @click="scrollPage('page-skill')" href="javascript:void(0);">skill</a></li>
+            <li class=""><a @click="scrollPage('page-works')" href="javascript:void(0);">works</a></li>
+            <li class=""><a @click="scrollPage('page-contact')" href="javascript:void(0);">contact</a></li>
+        </ul>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'AppFooter',
+    methods: {
+        scrollPage(sectionName) {
+            this.$emit("scroll", sectionName);
+        }
+    }
+}
+</script>
+
+<style scoped>
+.section--footer {
+    background-color: #000;
+}
+.menu {
+    display: flex;
+    justify-content: center;
+}
+.menu li + li::before {
+    content: "/";
+    padding: 10px;
+    color: #fff;
+}
+.menu li a {
+    text-decoration: none;
+    color: #fff;
+}
+</style>
