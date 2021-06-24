8ea6854fe0963bb784ae8dcea038f29cddc0a5ae
julia1450 <julia1450@naver.com>
2021-06-24 18:04:36 +0900

    fontawesome 아이콘을 추가한다

--- src/main.js
+++ src/main.js
@@ -2,7 +2,14 @@ import './assets/css/reset.css'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
+library.add(fas, far)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.use(router)
 Vue.config.productionTip = false
 
