81cea55706a34bc777e0f9d68919d8c98f50fc39
julia1450 <julia1450@naver.com>
2021-07-27 18:04:08 +0900

    라우터 링크를 추가한다

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import AppMain from '../components/AppMain'
+import WorkList from '../components/works/WorkList'
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
     {
         path: process.env.NODE_ENV === 'production'? '/portfolio' : '/',
-        component: AppMain
+        component: AppMain,
+        meta: { scrollToTop: true }
+    },
+    {
+        path: process.env.NODE_ENV === 'production'? '/portfolio/works' : '/works',
+        component: WorkList
     }
 ]
 
