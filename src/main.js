bab37e6cc62b64444642b05d74d7af7371093f6f
julia1450 <julia1450@naver.com>
2021-07-29 16:16:41 +0900

    fontawesome 브랜드 아이콘을 추가한다

--- src/main.js
+++ src/main.js
@@ -6,9 +6,10 @@ import { store } from './store/store'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(fas, far)
+library.add(fas, far, fab)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.use(router)
