959e133c802ff2214dc822d283d2520a04b23f75
hyewonyun <julia1450@naver.com>
2021-12-01 14:53:10 +0900

    코드의 일관성을 위해 eslint를 추가한다

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+	root: true,
+	env: {
+		node: true
+	},
+	plugins: ["prettier"],
+	extends: [
+		"eslint:recommended",
+		"plugin:vue/essential",
+		"prettier",
+		"plugin:prettier/recommended"
+	],
+	rules: {
+		"no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+		"no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+		"prettier/prettier": [
+			"error",
+			{
+				printWidth: 80,
+				semi: false,
+				useTabs: true,
+				tabWidth: 2,
+				traillingComma: "all",
+				barcketSpacing: true,
+				arrowParens: "avoid"
+			}
+		]
+	},
+	parserOptions: {
+		parser: "babel-eslint"
+	},
+	overrides: [
+		{
+			files: [
+				"**/__tests__/*.{j,t}s?(x)",
+				"**/tests/unit/**/*.spec.{j,t}s?(x)"
+			],
+			env: {
+				jest: true
+			}
+		}
+	]
+}
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-    ? '/portfolio'
-    : '/',
-    outputDir: './docs'
+	publicPath: process.env.NODE_ENV === "production" ? "/portfolio" : "/",
+	outputDir: "./docs",
+	devServer: {
+		overlay: false
+	}
 }
