2a3d4f097b648decdf7ce6ee36a937783e43c9b7
hyewonyun <julia1450@naver.com>
2021-12-02 15:10:41 +0900

    헤더와 푸터를 가지는 기본 레이아웃을 따로 분리한다

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,12 @@
 <template>
 	<div id="app">
-		<AppHeader />
 		<router-view />
-		<AppFooter />
 	</div>
 </template>
 
 <script>
-import AppHeader from "@/components/AppHeader.vue"
-import AppFooter from "@/components/AppFooter.vue"
-
 export default {
 	name: "App",
-	components: {
-		AppHeader,
-		AppFooter,
-	},
 	methods: {},
 }
 </script>
