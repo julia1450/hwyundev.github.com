893e6f973faa7cb42328ba66baecf7523aa15221
hyewonyun <julia1450@naver.com>
2021-12-02 17:51:55 +0900

    작업목록 페이지 create훅에서 api를 호출하도록한다

--- src/components/works/WorkList.vue
+++ src/components/works/WorkList.vue
@@ -1,11 +1,41 @@
 <template>
-	<div>WorkList</div>
+	<div class="section section--work">
+		<h1>WorkList</h1>
+		<div class="work-content-wrap">
+			<div class="work-list">
+				<WorkListItem v-for="work in works" :key="work.workId">{{
+					work.workTitle
+				}}</WorkListItem>
+			</div>
+			<div class="work-detail"></div>
+		</div>
+	</div>
 </template>
 
 <script>
+import { mapGetters } from "vuex"
+import WorkListItem from "@/components/works/WorkListItem"
 export default {
 	name: "WorkList",
+	components: {
+		WorkListItem,
+	},
+	created() {
+		this.$store.dispatch("fetchWorks")
+	},
+	computed: {
+		...mapGetters({
+			works: "getDashboardWorks",
+		}),
+	},
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.section--work {
+	height: calc(100vh - 180px);
+}
+.work-content-wrap {
+	display: flex;
+}
+</style>
