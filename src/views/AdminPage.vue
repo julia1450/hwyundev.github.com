757525e9048a3f8dc3c47c92bf56cca8603f1c4a
hyewonyun <julia1450@naver.com>
2022-02-16 15:23:07 +0900

    권한테스트를 위한 admin 화면을 추가한다

---
+++ src/views/AdminPage.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="admin-wrapper">관리자페이지</div>
+</template>
+
+<script>
+export default {
+	name: "AdminPage",
+	data() {
+		return {
+			baseUrl: process.env.NODE_ENV === "production" ? "/portfolio/" : "/",
+		}
+	},
+	methods: {},
+}
+</script>
+
+<style>
+.admin-wrapper {
+	background-color: #f8f4fe;
+	height: 100vh;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+</style>
